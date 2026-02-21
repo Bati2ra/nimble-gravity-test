dc295e662d9208768428c80c84348e2d99bc227b
BautistaPicca <95726581+Bati2ra@users.noreply.github.com>
2026-02-21 13:04:21 -0300

    fix: fetch catch

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -9,8 +9,11 @@ export async function fetchCandidate(email) {
         if (!res.ok) throw new Error(`Failed to fetch candidate (${res.status})`);
         return res.json();
     } catch (e) {
-        // En caso de error de red o url incorrecta se lanza un error genérico.
-        throw new Error("Network error. Please try again later.");
+        if (e instanceof TypeError) {
+            throw new Error("Network error. Please try again later.");
+        }
+
+        throw e;
     }
 }
 
@@ -20,18 +23,21 @@ export async function fetchJobs() {
         if (!res.ok) throw new Error(`Failed to fetch jobs (${res.status})`);
         return res.json();
     } catch (e) {
-        // En caso de error de red o url incorrecta se lanza un error genérico.
-        throw new Error("Network error. Please try again later.");
+        if (e instanceof TypeError) {
+            throw new Error("Network error. Please try again later.");
+        }
+
+        throw e;
     }
 }
 
 
 export async function submitApplication({
-        uuid,
-        candidateId,
-        jobId,
-        repoUrl,
-      }) {
+    uuid,
+    candidateId,
+    jobId,
+    repoUrl,
+}) {
     try {
         const res = await fetch(`${BASE_URL}/api/candidate/apply-to-job`, {
             method: "POST",
@@ -45,12 +51,14 @@ export async function submitApplication({
         });
 
         if (!res.ok) {
-            const err = await res.json().catch(() => ({}));
-            throw new Error(err.message || `Submission failed (${res.status})`);
+            throw new Error("Something went wrong. try again.");
         }
         return res.json();
     } catch (e) {
-        // En caso de error de red o url incorrecta se lanza un error genérico.
-        throw new Error("Network error. Please try again later.");
+        if (e instanceof TypeError) {
+            throw new Error("Network error. Please try again later.");
+        }
+
+        throw e;
     }
 }
